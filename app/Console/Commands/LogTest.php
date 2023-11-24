b7e9cf3c7355ea55c3ee0c0a1e8a7954a673f9b7
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2023-11-24 16:52:44 +0300

    Log Test Command

---
+++ app/Console/Commands/LogTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class LogTest extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'log:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Log::info('Test Log Message');
+    }
+}
