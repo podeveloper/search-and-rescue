b887f6e69119f8c7812f322a76d26dbb74b6a670
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2023-11-25 15:36:22 +0300

    Update User Migration and Database Seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,33 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname')->nullable();
+            $table->string('full_name')->nullable();
+            $table->string('phone')->nullable();
             $table->string('email')->unique();
+            $table->timestamp('last_login_at')->nullable();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('profile_photo')->nullable();
+            $table->date('date_of_birth')->nullable();
             $table->string('password');
+            $table->string('password_temp')->nullable();
+            $table->foreignId('gender_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('nationality_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('education_level_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('reference_name')->nullable();
+            $table->foreignId('reference_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->foreignId('referral_source_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('occupation_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('occupation_text')->nullable();
+            $table->foreignId('organisation_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('organisation_text')->nullable();
+            $table->string('marital_status')->nullable();
+            $table->boolean('is_admin')->default(false);
+            $table->boolean('kvkk')->default(true);
+            $table->longText('note')->nullable();
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(EssentialSeeder::class);
+        $this->call(CategoriesSeeder::class);
+        $this->call(PlacesSeeder::class);
+        // $this->call(InterviewAvailableTimeSeeder::class);
+        // $this->call(DemoDataSeeder::class);
     }
 }
