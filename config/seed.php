2bd13e6a0d744172daddcdd07796aca1bf2d9bd2
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2023-11-25 04:26:34 +0300

    Import Config

---
+++ config/seed.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Seed Config
+    |--------------------------------------------------------------------------
+    */
+
+    'admin' => [
+        'name' => env('ADMIN_NAME','Admin'),
+        'email' => env('ADMIN_EMAIL','login@admin.com'),
+        'password' => env('ADMIN_PASSWORD'),
+    ],
+
+];
