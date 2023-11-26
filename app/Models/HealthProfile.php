cddf44fcf072f1086c0f3ffaba1a5d4dd8657af3
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2023-11-26 05:50:17 +0300

    Health Profile Model

---
+++ app/Models/HealthProfile.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class HealthProfile extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'medications',
+        'allergies',
+        'medical_conditions',
+        'vision_aids',
+        'prosthetics',
+        'emergency_contact_name',
+        'emergency_contact_phone',
+        'blood_type',
+        'other_health_information',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2023_11_25_212214_create_health_profiles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('health_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->text('medications')->nullable();
+            $table->text('allergies')->nullable();
+            $table->text('medical_conditions')->nullable();
+            $table->text('vision_aids')->nullable();
+            $table->text('prosthetics')->nullable();
+            $table->string('emergency_contact_name')->nullable();
+            $table->string('emergency_contact_phone')->nullable();
+            $table->string('blood_type')->nullable();
+            $table->longText('other_health_information')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('health_profiles');
+    }
+};
