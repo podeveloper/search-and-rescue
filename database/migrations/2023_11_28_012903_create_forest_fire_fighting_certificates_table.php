cf7756cd2bcafeda7c48ebeaaccc8b6f5df63a6d
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2023-11-28 05:00:49 +0300

    Forest Fire Fighting Certificate Model & Migration

---
+++ database/migrations/2023_11_28_012903_create_forest_fire_fighting_certificates_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('forest_fire_fighting_certificates', function (Blueprint $table) {
+            $table->id();
+            $table->string('registration_number');
+            $table->foreignId('work_area_city_id')->nullable();
+            $table->string('directorate')->nullable();
+            $table->string('duty')->nullable();
+            $table->date('date_of_issue')->nullable();
+            $table->date('expiration_date')->nullable();
+            $table->string('pdf')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('forest_fire_fighting_certificates');
+    }
+};
