565365cc9d07b8c7c8a9aeeb5208fd1a794d63c8
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2023-11-30 01:02:53 +0300

    Get App Name as Brand Name For Providers

--- app/Providers/Filament/OfficialPanelProvider.php
+++ app/Providers/Filament/OfficialPanelProvider.php
@@ -30,7 +30,7 @@ class OfficialPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
-            ->brandName('MAKUD SAR')
+            ->brandName(env('APP_NAME'))
             ->brandLogo(asset(str_contains(request()->url(),'login') ? 'img/login-logo.jpg' : 'img/panel-logo.jpg'))
             ->brandLogoHeight(str_contains(request()->url(),'login') ? '150px' : '50px')
             ->favicon(asset('img/favicon-32x32.png'))
