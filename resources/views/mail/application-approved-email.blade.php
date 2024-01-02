1188d88daad7872fc12dd1812a003e0de9292171
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2024-01-03 02:01:28 +0300

    Application Approved & Rejected Emails

---
+++ resources/views/mail/application-approved-email.blade.php
@@ -0,0 +1,7 @@
+<h3> Sayın {!! ucwords($user->full_name ?? $user->name . ' ' . $user->surname) !!} </h3>
+<p>
+    Kabul Emaili
+</p>
+<h4>
+    Motosikletli Arama Kurtartma ve Destek Derneği
+</h4>
