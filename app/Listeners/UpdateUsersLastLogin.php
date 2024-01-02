179dee08561674ec63f4e3decdaef1386a10cbc9
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2024-01-03 01:59:22 +0300

    Update Users Last Login Listener

---
+++ app/Listeners/UpdateUsersLastLogin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Authenticated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class UpdateUsersLastLogin
+{
+    use InteractsWithQueue;
+
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Authenticated $event)
+    {
+        $user = $event->user;
+        if($user->last_login_at < now()->subHour()) $user->update(['last_login_at' => now()]);
+    }
+}
