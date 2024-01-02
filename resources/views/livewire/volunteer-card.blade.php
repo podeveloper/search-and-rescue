b01dc98ea2bc6613a9b4a044072eec487a55dc50
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2024-01-03 01:51:30 +0300

    Volunteer Card

---
+++ resources/views/livewire/volunteer-card.blade.php
@@ -0,0 +1,15 @@
+<div class="flex items-center justify-center">
+
+    <style>
+        .justify-between {
+            justify-content: center !important;
+        }
+
+        .items-center {
+            justify-content: center !important;
+        }
+    </style>
+    <div style="margin-top: 3rem; width: 80%">
+        {{ $this->volunteerInfolist }}
+    </div>
+</div>
